30b872796ec16cdc5c22f4ce02ef3ddb848f0e91
Jean Pierre <jeanpierre101298gmail.com>
2021-07-13 14:45:28 -0500

    Validación de permisos en tareas routes

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -5,6 +5,7 @@ const {body}=require('express-validator');
 const {handlerValidationErrors}=require('../middlewares/handlerValidationErrors');
 const auth=require('../middlewares/auth');
 const verifyAuthUser=require('../middlewares/verifyAuthUser');
+const {adminPermission,validatePermissions, JefeTallerPermission }=require('../middlewares/permissions');
 ///get one task
 router.get('/:id', 
     auth,
@@ -14,11 +15,17 @@ router.get('/:id',
 router.delete('/:id', 
     auth,
     verifyAuthUser,
+    adminPermission,
+    JefeTallerPermission,
+    validatePermissions,
     taskController.deleteTask);
 
 router.put('/:id', 
     auth,
     verifyAuthUser,
+    adminPermission,
+    JefeTallerPermission,
+    validatePermissions,
     body('ProyectoId').trim().notEmpty().escape().withMessage('El id del proyecto es requerido'),
     body('nombre').trim().notEmpty().escape().withMessage('Nombre no válido'),
     body('descripcion').trim().notEmpty().escape().isString().withMessage('Descripción no válida'),
