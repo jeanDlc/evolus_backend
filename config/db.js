6fd820dd690d0094968838642d8f260bdc502345
Jean Pierre <jeanpierre101298gmail.com>
2021-07-22 12:04:53 -0500

    agregando host en varaible de entorno

--- config/db.js
+++ config/db.js
@@ -1,16 +1,21 @@
-const {Sequelize}=require('sequelize');
-require('dotenv').config();
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql',
-  
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
-      idle: 10000
+      idle: 10000,
     },
-    logging:false
-});
+    logging: false,
+  }
+);
 
-module.exports= db;
+module.exports = db;
